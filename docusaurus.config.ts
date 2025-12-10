5a4d8e4a0290c2739e92f6b12717fa16d3c51a66
Muhammad Faizan Manzoor <fkaaa567@gmail.com>
2025-12-10 18:54:52 +0500

    update favicon icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Physical AI & Humanoid Robotics',
   tagline: 'Master the future of embodied intelligence',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/robot-favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
